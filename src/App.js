f4885383bdb3df496395b42e9e78b0d30074dd63
jitender1222 <93830004+jitender1222@users.noreply.github.com>
2022-11-08 23:24:13 +0530

    adding the routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter,Route,Routes } from "react-router-dom";
+
+import Home from './Component/Routes/Home';
+import About from './Component/Routes/About';
+import Product from './Component/Routes/Product';
+import Cart from './Component/Routes/Cart';
+import SingleProdcut from './Component/Routes/SingleProduct'
+import Contact from './Component/Routes/Contact';
+import Error from './Component/Routes/Error';
+import Header from './Component/Header/Header';
+
+// import { GlobalStyle } from './Component/Routes/StylesComponent/GlobalStyle';
+// import { ThemeProvider } from 'styled-components';
+
 function App() {
+
+  // const theme={
+  //   colors:{
+  //     bg:"#000"
+  //   }
+  // }
   return (
-    <div className="App">
-      Helo Jitender 
+    <div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+    <Route path='/' element={<Home />} ></Route>
+    <Route path='/about' element={<About />} ></Route>
+    <Route path='/product' element={<Product/>} ></Route>
+    <Route path='/contact' element={<Contact />} ></Route>
+    <Route path='/singleproduct:id' element={<SingleProdcut />} ></Route>
+    <Route path='/cart' element={<Cart />} ></Route>
+    <Route path='*' element={<Error />} ></Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
