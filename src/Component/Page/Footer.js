19cd85dae2be3545d1ea843c5b701f31871ab09b
jitender1222 <93830004+jitender1222@users.noreply.github.com>
2022-11-09 09:46:24 +0530

    adding footer

---
+++ src/Component/Page/Footer.js
@@ -0,0 +1,37 @@
+import "../Page/Pages.css"
+
+const Footer=()=>{
+    return (
+        <>
+        <div className="container">
+            <div className="semi-outer">
+                <p>Ready to get Started? <br/> Talk To us Today</p>  
+                <button className="btn footer-btn">Get Started</button>
+            </div>
+
+            {/* <br/>
+
+            <div className="inner-div">
+                <div className="first-col">
+                    <p>Jitender Singh</p>
+                </div>
+                <div className="sec-col">
+                    <p>Subscribe to get important updates </p>
+                    <input type="email" placeholder="Enter Your email" />
+                    <button>Subscribe</button>
+                </div>
+                <div className="third-col">
+                    <p>Call Us</p>
+                    <p>+9123456789</p>
+                </div>
+            </div>
+            <div className="last-section">
+                <p>&amp2022 Jitender Singh.All Right Reserved</p>
+                <p>Privacy Policy <br /> Terms & Conditions</p>
+            </div> */}
+        </div>
+        </>
+    )
+}
+
+export default Footer;
